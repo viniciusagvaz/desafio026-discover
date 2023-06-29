6233b005d4f31766abb2900e0b73ffaca3021754
Vinicius Vaz <agvazvinicius@gmail.com>
2023-06-29 17:00:15 -0300

    ingredients order fix

--- app.js
+++ app.js
@@ -13,20 +13,16 @@ Quantity = {
       Quantity.updateIngredients(500, 500, 250)
    },
 
+   formatIngredients(ingredientQuantity) {
+      let result = quantity * ingredientQuantity
+
+      return result >= 1000 ? `${result / 1000}kg` : `${result}g`
+   },
+
    updatePortions() {
       document
          .querySelector('.portionsQuantity span')
          .innerHTML = quantity < 10 ? `0${quantity}` : quantity
-   },
-
-   formatIngredients(ingredientQuantity) {
-      let result = quantity * ingredientQuantity
-
-      if (result >= 1000) {
-         return `${result / 1000}kg`
-      }
-
-      return `${result}g`
    },
 
    updateIngredients(i1, i2, i3) {
@@ -35,8 +31,8 @@ Quantity = {
       document
          .querySelector('ul')
          .innerHTML = `
-            <li>${Quantity.formatIngredients(i1)} de açúcar</li>
             <li>${Quantity.formatIngredients(i2)} de amendoim sem pele torrado</li>
+            <li>${Quantity.formatIngredients(i1)} de açúcar</li>
             <li>${Quantity.formatIngredients(i3)} de farinha de milho flocada</li>
             <li>${quantity} ${spoon} (café) de sal</li>
           `
