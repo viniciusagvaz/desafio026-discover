6cb0a863247635932a9872e06a27da4dfd7c3959
viniciusagvaz <agvazvinicius@gmail.com>
2023-12-11 19:03:22 -0300

    paçoca shadow

--- app.js
+++ app.js
@@ -1,43 +1,44 @@
-let quantity = 1
+let quantity = 1;
 
 Quantity = {
-   less() {
-      quantity > 1 ? quantity-- : quantity
-      Quantity.updateQuantities()
-   },
+	less() {
+		quantity > 1 ? quantity-- : quantity;
+		Quantity.updateQuantities();
+	},
 
-   more() {
-      quantity < 99 ? quantity++ : quantity
-      Quantity.updateQuantities()
-   },
+	more() {
+		quantity < 99 ? quantity++ : quantity;
+		Quantity.updateQuantities();
+	},
 
-   formatIngredients(ingredientQuantity) {
-      let result = quantity * ingredientQuantity
+	formatIngredients(ingredientQuantity) {
+		const result = quantity * ingredientQuantity;
 
-      return result >= 1000 ? `${result / 1000}kg` : `${result}g`
-   },
+		return result >= 1000 ? `${result / 1000}kg` : `${result}g`;
+	},
 
-   updateRecipes() {
-      document
-         .querySelector('.recipesQuantity span')
-         .innerHTML = quantity < 10 ? `0${quantity}` : quantity
-   },
+	updateRecipes() {
+		document.querySelector(".recipesQuantity span").innerHTML =
+			quantity < 10 ? `0${quantity}` : quantity;
+	},
 
-   updateIngredients(i1, i2, i3) {
-      let spoon = quantity > 1 ? `colheres` : `colher`
+	updateIngredients(i1, i2, i3) {
+		const spoon = quantity > 1 ? `colheres` : `colher`;
 
-      document
-         .querySelector('ul')
-         .innerHTML = `
-            <li>${Quantity.formatIngredients(i2)} de amendoim sem pele torrado</li>
+		document.querySelector("ul").innerHTML = `
+            <li>${Quantity.formatIngredients(
+							i2
+						)} de amendoim sem pele torrado</li>
             <li>${Quantity.formatIngredients(i1)} de açúcar</li>
-            <li>${Quantity.formatIngredients(i3)} de farinha de milho flocada</li>
+            <li>${Quantity.formatIngredients(
+							i3
+						)} de farinha de milho flocada</li>
             <li>${quantity} ${spoon} (café) de sal</li>
-          `
-   },
+          `;
+	},
 
-   updateQuantities() {
-      Quantity.updateRecipes()
-      Quantity.updateIngredients(500, 500, 250)
-   }
-}
+	updateQuantities() {
+		Quantity.updateRecipes();
+		Quantity.updateIngredients(500, 500, 250);
+	},
+};
